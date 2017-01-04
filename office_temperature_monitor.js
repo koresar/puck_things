695bf225e5b369cea7488e9f778d74909d8c485c
Vasyl Boroviak <kore.sar@gmail.com>
2017-01-04 19:38:21 +1100

    First version of office temperature monitor

---
+++ office_temperature_monitor.js
@@ -0,0 +1,61 @@
+var colors = { RED: LED1, GREEN: LED2, BLUE: LED3 };
+function turnLedsOff() {
+  colors.RED.reset();
+  colors.GREEN.reset();
+  colors.BLUE.reset();
+}
+
+var intervalId, state = 0, isBlinking = false;
+function stopBlinking(LED) {
+  if (!isBlinking) return;
+
+  isBlinking = false;
+  clearInterval(intervalId);
+  turnLedsOff();
+}
+function startBlinking(LED, interval) {
+  LED = LED || colors.RED;
+  if (isBlinking === LED) return;
+
+  stopBlinking();
+
+  state = 0;
+  isBlinking = LED;
+  interval = interval || 1000;
+  intervalId = setInterval(intervalCallback, interval);
+}
+function intervalCallback() {
+  if (!isBlinking) return;
+  state = (state + 1) % 2;
+  isBlinking.write(state);
+  if (state) setTimeout(intervalCallback, 100);
+}
+
+function checkTemperature() {
+  var temperature = E.getTemperature() + 2; // accuracy offset
+
+  if (temperature <= 22) startBlinking(colors.BLUE);
+  else if (temperature >= 25) startBlinking(colors.RED);
+  else stopBlinking();
+}
+var monitoringId, isMonitoring;
+function startMonitoring() {
+  isMonitoring = true;
+  monitoringId = setInterval(checkTemperature, 5000);
+}
+function stopMonitoring() {
+  clearInterval(monitoringId);
+  stopBlinking();
+  isMonitoring = false;
+  setDeepSleep(true);
+}
+
+turnLedsOff();
+checkTemperature();
+startMonitoring();
+setDeepSleep(true);
+
+setWatch(function() {
+  if (isMonitoring) stopMonitoring();
+  else startMonitoring();
+}, BTN, {edge:"rising", debounce:50, repeat:true});
