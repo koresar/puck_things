c5aa12d4458351bec71591e56b7d3efbb977b6d4
Vasyl Boroviak <kore.sar@gmail.com>
2018-07-13 23:34:03 +1000

    Create puck.js

---
+++ comm_temp/puck.js
@@ -0,0 +1,37 @@
+class Temperatures {
+  constructor(max) {
+    this.max = max || 10;
+    this.temps = [];
+    this._pos = 0;
+    this._sum = 0;
+  }
+  
+  push(t) {
+    if (this.temps.length < this.max) {
+      this.temps.push(t);
+      this._sum += t;
+    } else {
+      this._sum -= this.temps[this._pos];
+      this._sum += t;
+      this.temps[this._pos] = t;
+      this._pos = (this._pos+1) % this.max;
+    }
+    
+    return this._sum / this.temps.length;
+  }
+}
+
+var lastTemps = new Temperatures();
+function updateBT() {
+  const t = lastTemps.push(E.getTemperature());
+  const n = Math.floor(t);
+  const f = Math.round((t - n) * 100);
+  //console.log(t, n, f);
+  NRF.setAdvertising({}, {
+    manufacturer: 0x590,
+    manufacturerData: [Puck.getBatteryPercentage(), n, f]
+  });
+}
+
+setInterval(updateBT, 3000);
+updateBT();
