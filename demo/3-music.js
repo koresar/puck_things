adcddad7e447e33a365f41e35f90fe901c69d2e6
vasyl-ffx <vasyl@flash-fx.com.au>
2018-08-07 12:53:11 +1000

    Long click to mute for 25 sec (skip spotify ads).

--- demo/3-music.js
+++ demo/3-music.js
@@ -1,13 +1,20 @@
 var controls = require("ble_hid_controls");
-NRF.setServices(undefined, { hid : controls.report });
+NRF.setServices(undefined, { hid: controls.report });
 
-setWatch(function(e) {
+setWatch(function (e) {
   var len = e.time - e.lastTime;
-  if (len > 0.3) {
+  if (len > 1) {
+    controls.mute();
+    digitalPulse(LED3, 1, 100);
+    setTimeout(function () {
+      controls.mute();
+      digitalPulse(LED3, 1, 100);
+    }, 25000);
+  } else if (len > 0.3) {
     controls.next();
-    digitalPulse(LED1,1,100);
+    digitalPulse(LED1, 1, 100);
   } else {
     controls.playpause();
-    digitalPulse(LED2,1,100);
+    digitalPulse(LED2, 1, 100);
   }
-}, BTN, { edge:"falling",repeat:true,debounce:50});
+}, BTN, { edge: "falling", repeat: true, debounce: 50 });
